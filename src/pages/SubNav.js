4fb7f2ed68e2c850a3acdf3a8a2c1864c96e82c8
Seungji <96293047+tuz22@users.noreply.github.com>
2022-11-18 02:17:51 +0900

    sub-nav 컴포넌트 분리

---
+++ src/pages/SubNav.js
@@ -0,0 +1,44 @@
+function SubNav() {
+  return (
+    <div className='hidden'>
+      <div className='nav-container'>
+        <header>
+          <h2>앗!</h2>
+          <a>로그인이 필요해요</a>
+        </header>
+        <nav>
+          <h2>테마</h2>
+          <ul>
+            <li>전체보기</li>
+            <li>목장갑은 뚝딱 만들어지지 않았다</li>
+            <li>나의 첫 배민문방구</li>
+            <li>[단독] 베스트셀러 출생의 비밀</li>
+          </ul>
+          <h2><span>카테고리</span></h2>
+          <ul>
+            <li>전체보기</li>
+            <li>~</li>
+            <li>명예의 전당</li>
+          </ul>
+        </nav>
+        <footer>
+          <div>
+            <h2>고객센터</h2>
+            <div>1:1 문의</div>
+            <div>|</div>
+            <div>이메일 문의</div>
+
+            {/* <ul>
+              <li>1:1 문의</li>
+              <li>이메일 문의</li>
+            </ul> */}
+          </div>
+        </footer>
+        <div className='close-btn'>닫기</div>
+      </div>
+      <div className='nav-bg'/>
+    </div>
+  )
+}
+
+export default SubNav
