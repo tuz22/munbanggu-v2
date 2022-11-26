bed2d204800704b9e224b114a6fc9c61f86bbaab
Seungji <96293047+tuz22@users.noreply.github.com>
2022-11-26 17:14:41 +0900

    footer CSS 추가

--- src/App.js
+++ src/App.js
@@ -106,24 +106,36 @@ function App() {
         </article>
       </section>
       <footer>
-        <img src="" alt="로고" />
-        <a href='#'>About</a>
-        <a href='#'>공지사항</a>
-        <a href='#'>이용약관</a>
-        <a href='#'><b>개인정보처리방침</b></a>
-        <a href='#'>대량구매/제휴안내</a>
-        <img src="" alt="" />
-        <a href='#'>@baemin_store</a>
-        <div>
-          상호 : (주)우아한형제들 대표 : 김범준
-          사업자등록번호 : 120-87-65763
-          통신판매업신고번호 : 2021-서울송파-0515
-          <a href='#'>사업자정보확인</a>
-          대표번호 : 1670-9902
-          이메일 : marketing_store@woowahan.com
-          주소 : 서울특별시 송파구 위례성대로 2 장은빌딩
-          호스팅제공 : (주)우아한형제들
-          © Woowa Brothers Corp. All rights reserved
+        <div className='footer-container'>
+          <h2 className='footer-logo'>
+            <span>배민문방구</span>
+          </h2>
+          <div>
+            <nav className='footer-nav'>
+              <ul>
+                <li><a href='#'>About</a></li>
+                <li><a href='#'>공지사항</a></li>
+                <li><a href='#'>이용약관</a></li>
+                <li><a href='#'><b>개인정보처리방침</b></a></li>
+                <li><a href='#'>대량구매/제휴안내</a></li>
+              </ul>
+              <p className='link-sns'>
+                <a href='https://www.instagram.com/baemin_store/?hl=ko' target='_blank'>@baemin_store</a>
+              </p>
+            </nav>
+            <div className='footer-content'>
+              <p>상호 : (주)우아한형제들</p>
+              <p>대표 : 김범준</p>
+              <p>사업자등록번호 : 120-87-65763</p>
+              <p>통신판매업신고번호 : 2021-서울송파-0515</p>
+              <p>사업자정보확인</p>
+              <p>대표번호 : 1670-9902</p>
+              <p>이메일 : marketing_store@woowahan.com</p>
+              <p>주소 : 서울특별시 송파구 위례성대로 2 장은빌딩</p>
+              <p>호스팅제공 : (주)우아한형제들</p>
+              <p>© Woowa Brothers Corp. All rights reserved</p>
+            </div>
+          </div>
         </div>
       </footer>
     </div>
