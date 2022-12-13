4251c8b549687e549edd9c37ea28ea3318ca5e9d
Seungji <96293047+tuz22@users.noreply.github.com>
2022-12-13 14:30:05 +0900

    Payment 화면 오류 수정, 빈 장바구니 페이지 추가

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -26,13 +26,13 @@ function Cart(){
         <section className='cart-box'>
         {/* 선택한 상품 */}
           <article className="cart-content">
-            <div className='cart-header'>
+            {/* <div className='cart-header'>
               <div className='check-box'>
                 <input type='checkbox' id='@checkAll' />
                 <label htmlFor='@checkAll'>전체선택(0/0)</label>
               </div>
               <button className='select-del-btn'>선택삭제</button>
-            </div>
+            </div> */}
             <CartItem />
           </article>
           {/* 총 상품 금액 */}
@@ -49,48 +49,68 @@ function CartItem(){
   const navigate = useNavigate();
   
   return (
-    state && state.map((a, i) => {
-      const cartItemId = state[i].id
-
-      return (
-        <ul className='cart-list' key={i}>
-          <li>
-            <div className='check-box'>
-              <input type='checkbox' id='@check0' />
-              <label htmlFor='@check0' />
-            </div>
-            <div className="list-box">
-              <div className="thumbnail">
-                <a onClick={() => {navigate('/goods/detail/' + state[i].id)}} >
-                  <img src={state[i].thumbnail1} alt="" />
-                </a>
-              </div>
-              <div className='list-info'>
-                <p className="list-name">
-                <a onClick={() => {navigate('/goods/detail/' + state[i].id)}} >
-                  {state[i].title}
-                </a>
-                </p>
-              </div>
-            </div>
-            <div className="cart-info">
-              <div className="quantity">
-                <input type="text" value={state[i].count}/>
-                <button className='btn-minus' onClick={() => {dispatch(decrease(cartItemId))}}>-</button>
-                <button className='btn-plus' onClick={() => {dispatch(increase(cartItemId))}}>+</button>
-              </div>
-              <div className='price-info'>
-                <div className="price">
-                  {/* <span>{state[i].id}</span> */}
-                  <span>{state[i].price * state[i].count}원</span>
+    <>
+      {state.length > 0 
+      ? state && state.map((a, i) => {
+          const cartItemId = state[i].id
+          return (
+            <>
+              <div className='cart-header'>
+                <div className='check-box'>
+                  <input type='checkbox' id='@checkAll' />
+                  <label htmlFor='@checkAll'>전체선택(0/0)</label>
                 </div>
-                <button className='cart-del-btn'>삭제</button>
+                <button className='select-del-btn'>선택삭제</button>
               </div>
-            </div>
-          </li>
-        </ul>
-      )
-    })
+              <ul className='cart-list' key={i}>
+                <li>
+                  <div className='check-box'>
+                    <input type='checkbox' id='@check0' />
+                    <label htmlFor='@check0' />
+                  </div>
+                  <div className="list-box">
+                    <div className="thumbnail">
+                      <a onClick={() => {navigate('/goods/detail/' + state[i].id)}} >
+                        <img src={state[i].thumbnail1} alt="" />
+                      </a>
+                    </div>
+                    <div className='list-info'>
+                      <p className="list-name">
+                      <a onClick={() => {navigate('/goods/detail/' + state[i].id)}} >
+                        {state[i].title}
+                      </a>
+                      </p>
+                    </div>
+                  </div>
+                  <div className="cart-info">
+                    <div className="quantity">
+                      <input type="text" value={state[i].count}/>
+                      <button className='btn-minus' onClick={() => {dispatch(decrease(cartItemId))}}>-</button>
+                      <button className='btn-plus' onClick={() => {dispatch(increase(cartItemId))}}>+</button>
+                    </div>
+                    <div className='price-info'>
+                      <div className="price">
+                        <span>{state[i].price * state[i].count}원</span>
+                      </div>
+                      <button className='cart-del-btn'>삭제</button>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </>
+          )
+        })
+      : <>
+          <div className='cart-header' />
+          <ul>
+            <li className='cart-item-none'>
+              <h2 className='font'>앗!</h2>
+              <span className='text'>장바구니가 텅~</span>
+            </li>
+          </ul>
+        </>
+      }
+    </>
   )
 }
 
@@ -98,23 +118,49 @@ function Payment(){
   let state = useSelector((state) => state.cartItem)
 
   return (
-    state && state.map((a, i) => {
-      const amountPrice = state[i].price * state[i].count;
-      const shipping = 3000;
-      const tatoalPrice = amountPrice + shipping;
-      return (
-        <article className='payment-box'>
+    <>
+      {state.length > 0 
+      ? state && state.map((a, i) => {
+        const amountPrice = state[i].price * state[i].count;
+        const shipping = 3000;
+        const tatoalPrice = amountPrice + shipping;
+        return (
+          <article className='payment-box'>
+            <div className='box-sticky'>
+              <div className="payment-result">
+                <dl className='amount'>
+                  <dt className='amount-item'>총 상품금액</dt>
+                  <dd className='amount-price'>{amountPrice}원</dd>
+                  <dt>배송비</dt>
+                  <dd>+{amountPrice == 0 ? 0 : amountPrice >= 30000 ? 0 : shipping}원</dd>
+                </dl>
+                <dl className='total'>
+                  <dt>결제예상금액</dt>
+                  <dd>{amountPrice == 0 ? 0 : amountPrice < 30000 ? tatoalPrice : tatoalPrice - shipping}원</dd>
+                </dl>
+              </div>
+              <div className="order-box">
+                <button className='order-btn'>
+                  <strong></strong>
+                  주문하기
+                </button>
+              </div>
+            </div>
+          </article>
+        )
+      }) 
+      : <article className='payment-box'>
           <div className='box-sticky'>
             <div className="payment-result">
               <dl className='amount'>
                 <dt className='amount-item'>총 상품금액</dt>
-                <dd className='amount-price'>{amountPrice}원</dd>
+                <dd className='amount-price'>0원</dd>
                 <dt>배송비</dt>
-                <dd>+{amountPrice == 0 ? 0 : amountPrice >= 30000 ? 0 : shipping}원</dd>
+                <dd>+0원</dd>
               </dl>
               <dl className='total'>
                 <dt>결제예상금액</dt>
-                <dd>{amountPrice == 0 ? 0 : amountPrice < 30000 ? tatoalPrice : tatoalPrice - shipping}원</dd>
+                <dd>0원</dd>
               </dl>
             </div>
             <div className="order-box">
@@ -125,8 +171,8 @@ function Payment(){
             </div>
           </div>
         </article>
-      )
-    })
+      }
+    </>
   )
 }
 export default Cart
