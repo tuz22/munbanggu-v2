8450120f124b164a424dd480a22d85568f52b72d
Seungji <96293047+tuz22@users.noreply.github.com>
2022-12-13 17:22:10 +0900

    전체 체크박스 연동

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import data from '../data';
 import { decrease, increase } from '../store';
 
 let Container = styled.div`
@@ -26,13 +27,6 @@ function Cart(){
         <section className='cart-box'>
         {/* 선택한 상품 */}
           <article className="cart-content">
-            {/* <div className='cart-header'>
-              <div className='check-box'>
-                <input type='checkbox' id='@checkAll' />
-                <label htmlFor='@checkAll'>전체선택(0/0)</label>
-              </div>
-              <button className='select-del-btn'>선택삭제</button>
-            </div> */}
             <CartItem />
           </article>
           {/* 총 상품 금액 */}
@@ -43,65 +37,73 @@ function Cart(){
 }
 
 function CartItem(){
-  let state = useSelector((state) => state.cartItem)
-  let dispatch = useDispatch()
-  const [count, setCount] = useState(0)
+  let state = useSelector((state) => state.cartItem);
+  let dispatch = useDispatch();
+  const [count, setCount] = useState(0);
   const navigate = useNavigate();
-  
+  const checkItem = [];
+  const [checkCount, setCheckCount] = useState(0);
   return (
     <>
       {state.length > 0 
-      ? state && state.map((a, i) => {
-          const cartItemId = state[i].id
-          return (
-            <>
-              <div className='cart-header'>
-                <div className='check-box'>
-                  <input type='checkbox' id='@checkAll' />
-                  <label htmlFor='@checkAll'>전체선택(0/0)</label>
-                </div>
-                <button className='select-del-btn'>선택삭제</button>
-              </div>
-              <ul className='cart-list' key={i}>
-                <li>
-                  <div className='check-box'>
-                    <input type='checkbox' id='@check0' />
-                    <label htmlFor='@check0' />
-                  </div>
-                  <div className="list-box">
-                    <div className="thumbnail">
-                      <a onClick={() => {navigate('/goods/detail/' + state[i].id)}} >
-                        <img src={state[i].thumbnail1} alt="" />
-                      </a>
-                    </div>
-                    <div className='list-info'>
-                      <p className="list-name">
-                      <a onClick={() => {navigate('/goods/detail/' + state[i].id)}} >
-                        {state[i].title}
-                      </a>
-                      </p>
-                    </div>
-                  </div>
-                  <div className="cart-info">
-                    <div className="quantity">
-                      <input type="text" value={state[i].count}/>
-                      <button className='btn-minus' onClick={() => {dispatch(decrease(cartItemId))}}>-</button>
-                      <button className='btn-plus' onClick={() => {dispatch(increase(cartItemId))}}>+</button>
-                    </div>
-                    <div className='price-info'>
-                      <div className="price">
-                        <span>{state[i].price * state[i].count}원</span>
+      ? <div>
+          <div className='cart-header'>
+            <div className='check-box'>
+              {/* <input type='checkbox' id='@checkAll' /> */}
+              <input type='checkbox' id='@checkAll' onClick={() => {checkCount == 0 ? setCheckCount(-1) : setCheckCount(0) }} checked={checkCount == 0 ? true : false}/>
+              <label htmlFor='@checkAll'>전체선택</label>
+            </div>
+            <button className='select-del-btn'>선택삭제</button>
+          </div>
+          { state && state.map((a, i) => {
+              const cartItemId = state[i].id
+              const checkId = '@check' + i
+              checkItem.push(i)
+              console.log(checkItem)
+              console.log(checkCount)
+              return (
+                <div>
+                  <ul className='cart-list' key={i}>
+                    <li>
+                      <div className='check-box'>
+                        <input type='checkbox' id={checkId} onClick={() => {checkCount == 0 ? setCheckCount(-1) : setCheckCount(0)}} checked={checkCount == 0 ? true : false}/>
+                        <label htmlFor={checkId} />
                       </div>
-                      <button className='cart-del-btn'>삭제</button>
-                    </div>
-                  </div>
-                </li>
-              </ul>
-            </>
-          )
-        })
+                      <div className="list-box">
+                        <div className="thumbnail">
+                          <a onClick={() => {navigate('/goods/detail/' + state[i].id)}} >
+                            <img src={state[i].thumbnail1} alt="" />
+                          </a>
+                        </div>
+                        <div className='list-info'>
+                          <p className="list-name">
+                          <a onClick={() => {navigate('/goods/detail/' + state[i].id)}} >
+                            {state[i].title}
+                          </a>
+                          </p>
+                        </div>
+                      </div>
+                      <div className="cart-info">
+                        <div className="quantity">
+                          <input type="text" value={state[i].count}/>
+                          <button className='btn-minus' onClick={() => {dispatch(decrease(cartItemId))}}>-</button>
+                          <button className='btn-plus' onClick={() => {dispatch(increase(cartItemId))}}>+</button>
+                        </div>
+                        <div className='price-info'>
+                          <div className="price">
+                            <span>{state[i].price * state[i].count}원</span>
+                          </div>
+                          <button className='cart-del-btn'>삭제</button>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              )
+            })
+          }
+        </div>
       : <>
-          <div className='cart-header' />
           <ul>
             <li className='cart-item-none'>
               <h2 className='font'>앗!</h2>
