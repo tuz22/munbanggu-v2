610622ebae547842edbcc585ba858cb430f31cb8
Seungji <96293047+tuz22@users.noreply.github.com>
2022-12-16 23:38:28 +0900

    스크롤 top 설정

--- src/index.js
+++ src/index.js
@@ -6,19 +6,18 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import store from './store.js'
 import { Provider } from 'react-redux';
+import ScrollTop from './component/ScrollTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
   <Provider store={store}>
     <BrowserRouter>
+      <ScrollTop />
       <App />
     </BrowserRouter>
   </Provider>
   // </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
