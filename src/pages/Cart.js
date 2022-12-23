700dc3616983bdd7adcff4bd8f97284860b2ce88
Seungji <96293047+tuz22@users.noreply.github.com>
2022-12-23 23:57:41 +0900

    Cart 오류 수정

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import data from '../data';
 import { decrease, increase, dropItem } from './../store/cartSlice';
 import { checkToggle } from './../store/checkSlice';
 
@@ -17,8 +16,8 @@ const Container = styled.div`
 `
 const CartBtn = styled.button`
   color : ${props => props.color};
-  background : ${props => props.color == '#CCC' ? '#EEE' : '#2AC1BC'};
-  border-color : ${props => props.color == '#CCC' ? '#EEE' : '#2AC1BC'};
+  background : ${props => props.color === '#CCC' ? '#EEE' : '#2AC1BC'};
+  border-color : ${props => props.color === '#CCC' ? '#EEE' : '#2AC1BC'};
 `
 
 function Cart(){
@@ -31,7 +30,7 @@ function Cart(){
         </div>
       </header>
         <section className='cart-box'>
-        {/* 선택한 상품 */}
+          {/* 선택한 상품 */}
           <article className="cart-content">
             <CartItem />
           </article>
@@ -54,7 +53,7 @@ function CartItem(){
       ? <div>
           <div className='cart-header'>
             <div className='check-box'>
-              <input type='checkbox' id='@checkAll' onClick={() => {dispatch(checkToggle())}} checked={checkCount == 0 ? true : false}/>
+              <input type='checkbox' id='@checkAll' onClick={() => {dispatch(checkToggle())}} checked={checkCount === 0 ? true : false}/>
               <label htmlFor='@checkAll'>전체선택</label>
             </div>
             <button className='select-del-btn'>선택삭제</button>
@@ -68,20 +67,20 @@ function CartItem(){
                   <ul className='cart-list' key={i}>
                     <li>
                       <div className='check-box'>
-                        <input type='checkbox' id={checkId} onClick={() => {dispatch(checkToggle())}} checked={checkCount == 0 ? true : false}/>
+                        <input type='checkbox' id={checkId} onClick={() => {dispatch(checkToggle())}} checked={checkCount === 0 ? true : false}/>
                         <label htmlFor={checkId} />
                       </div>
                       <div className="list-box">
                         <div className="thumbnail">
-                          <a onClick={() => {navigate('/goods/detail/' + state[i].id)}} >
+                          <button onClick={() => {navigate('/goods/detail/' + state[i].id)}} >
                             <img src={state[i].thumbnail1} alt="" />
-                          </a>
+                          </button>
                         </div>
                         <div className='list-info'>
                           <p className="list-name">
-                          <a onClick={() => {navigate('/goods/detail/' + state[i].id)}} >
+                          <button onClick={() => {navigate('/goods/detail/' + state[i].id)}} >
                             {state[i].title}
-                          </a>
+                          </button>
                           </p>
                         </div>
                       </div>
@@ -129,7 +128,7 @@ function Payment(){
   if (state.length > 0) {
     state && state.map((a, i) => {
       let amountPrice = (state[i].price * state[i].count)
-      priceArr.push(amountPrice)
+      return priceArr.push(amountPrice)
     })
   } else {
     sumPrice = 0;
@@ -150,15 +149,15 @@ function Payment(){
             <dt className='amount-item'>총 상품금액</dt>
             <dd className='amount-price'>{sumPrice.toLocaleString()}원</dd>
             <dt>배송비</dt>
-            <dd>+{sumPrice == 0 ? 0 : sumPrice >= 30000 ? 0 : SHIPPING}원</dd>
+            <dd>+{sumPrice === 0 ? 0 : sumPrice >= 30000 ? 0 : SHIPPING}원</dd>
           </dl>
           <dl className='total'>
             <dt>결제예상금액</dt>
-            <dd>{sumPrice == 0 ? 0 : sumPrice >= 30000 ? sumPrice.toLocaleString() : (sumPrice+shipping).toLocaleString()}원</dd>
+            <dd>{sumPrice === 0 ? 0 : sumPrice >= 30000 ? sumPrice.toLocaleString() : (sumPrice+shipping).toLocaleString()}원</dd>
           </dl>
         </div>
         <div className="order-box">
-          <CartBtn className='order-btn' color={checkCount == 0 ? 'white' : '#CCC'}>
+          <CartBtn className='order-btn' color={checkCount === 0 && sumPrice !== 0 ? 'white' : '#CCC'}>
             <strong></strong>
             주문하기
           </CartBtn>
