407d9d27fbee539fbe3dbde52b4c3a48cf62a948
Seungji <96293047+tuz22@users.noreply.github.com>
2022-12-31 15:12:41 +0900

    배지 수정

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
-import { useParams, useNavigate, Link } from 'react-router-dom';
 import { useContext, useEffect, useRef, useState } from 'react';
-import { Context } from './../App.js';
+import { useParams, Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { addItem, increase } from './../store/cartSlice';
+import { Context } from './../App.js';
 
 const Container = styled.div`
   width : 1200px;
@@ -87,15 +87,13 @@ function Detail(props) {
   return (
     <div className='detail-container'>
       <Container>
-        {/* <h4>detail 페이지</h4> */}
-        {/* <h4>{itemId.title}</h4> */}
         <header className='detail-header'>
           <div className='item-info'>
             <div className='badge'>
               <span className='discount'>
                 { discount === null ? '' : discount + '% SALE' }
               </span>
-              <BadgeState className='badge-name' color={itemId.state === 'NEW' ? '#2AC1BC' : '#6236FF'}>
+              <BadgeState className='badge-name' color={itemId.state === 'NEW' ? '#2AC1BC' : itemId.state === 'GREEN' ? '#0c952a' : '#6236FF'}>
                 { itemId.state === '' ? '' : itemId.state }
               </BadgeState>
             </div>
@@ -204,9 +202,9 @@ function TabContent({tab, id}){
     <ViewBox>
       { [ 
           <ItemInfo>
-            <img src={itemInfo.content1} alt="" />
-            <img src={itemInfo.content2} alt="" />
-            <img src={itemInfo.content3} alt="" />
+            <img src={itemInfo.content1} alt="상품상세이미지1" />
+            <img src={itemInfo.content2} alt="상품상세이미지2" />
+            <img src={itemInfo.content3} alt="상품상세이미지3" />
           </ItemInfo>,
           <BasicGuid>
             <h4 className='guide-title'>배송안내</h4>
