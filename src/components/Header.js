e67327ff81ba2561df4a23d95552a9358b88ec7c
tuz22 <seungji.dev@gmail.com>
2023-08-24 23:52:15 +0900

    Cart 배지 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,39 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { subChange } from '../store/subSlice';
+import styled from 'styled-components';
+
+const CartBadge = styled.div`
+    background: ${(props) => props.bg};
+    width: 15px;
+    height: 15px;
+    margin-left: -14px;
+    border-radius: 50%;
+    text-align: center;
+    font-size: 12px;
+    z-index: 1;
+    opacity: 0.8;
+`;
+const CartBadgeCount = styled.span`
+    opacity: 2;
+    color: black;
+    font-family: hannaPro;
+`;
 
 function Header(props) {
     const { category } = props;
+    const cartItemLength = useSelector((state) => state.cartItem.length);
     const dispatch = useDispatch();
     const [bg, setBg] = useState('bgOff');
     const handleScroll = () => {
         window.scrollY === 0 ? setBg('bgOff') : setBg('bgOn');
+    };
+
+    const handleCartCount = () => {
+        if (cartItemLength > 0) {
+            return cartItemLength;
+        }
     };
 
     useEffect(() => {
@@ -45,11 +70,14 @@ function Header(props) {
                         <li>
                             <button className="icon search-btn">검색</button>
                         </li>
-                        <li>
-                            <Link to="/cart" className="icon cart-btn">
-                                장바구니
-                            </Link>
-                        </li>
+                        <Link to="/cart">
+                            <li className="cart-btn-wrap">
+                                <div className="icon cart-btn">장바구니 배지</div>
+                                <CartBadge bg={cartItemLength > 0 ? '#2ac1bc' : ''}>
+                                    <CartBadgeCount>{handleCartCount()}</CartBadgeCount>
+                                </CartBadge>
+                            </li>
+                        </Link>
                         <li className="login-btn">로그인</li>
                         <li>
                             <button
