5f4114d1bf16c446f8cf274308f921add021ca9a
tuz22 <seungji.dev@gmail.com>
2023-09-09 22:24:45 +0900

    베너 데이터 추가

---
+++ src/data/mainBanner.js
@@ -0,0 +1,28 @@
+const mainBanner = [
+    {
+        id: 0,
+        img: '/assets/img/banner1.jpg',
+        link: '/goods/detail/0',
+    },
+    {
+        id: 1,
+        img: '/assets/img/banner2.jpg',
+        link: '/goods/detail/1',
+    },
+    {
+        id: 2,
+        img: '/assets/img/banner3.jpg',
+        link: '/goods/detail/4',
+    },
+    {
+        id: 3,
+        img: '/assets/img/banner4.jpg',
+        link: '/goods/detail/6',
+    },
+    {
+        id: 4,
+        img: '/assets/img/banner5.png',
+        link: '/goods/detail/12',
+    },
+];
+export default mainBanner;
