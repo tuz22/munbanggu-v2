dab179e2b0520e914b52e84f34cb4c70d5c5dcc0
tuz22 <seungji.dev@gmail.com>
2023-09-14 22:34:47 +0900

    장바구니 삭제 수정

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -1,49 +1,62 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const cartSlice = createSlice({
-  name :'cartItem',
-  initialState : [],
-  reducers : {
-    addItem(state, action){
-      state.push(action.payload)
+    name: 'cartItem',
+    initialState: [],
+    reducers: {
+        addItem(state, action) {
+            state.push(action.payload);
+        },
+        dropItem(state, action) {
+            let cartItemId = state.findIndex((data) => {
+                return data.id === action.payload;
+            });
+            if (cartItemId) {
+                return state.filter((data) => data.id !== action.payload);
+            } else {
+                return state;
+            }
+        },
+        increase(state, action) {
+            let cartItemId = state.findIndex((data) => {
+                return data.id === action.payload;
+            });
+            state[cartItemId].count++;
+        },
+        decrease(state, action) {
+            let cartItemId = state.findIndex((data) => {
+                return data.id === action.payload;
+            });
+            if (state[cartItemId].count > 0) {
+                state[cartItemId].count--;
+            }
+        },
+        checkItem(state, action) {
+            let cartItemId = state.findIndex((data) => {
+                return data.id === action.payload;
+            });
+            if (state[cartItemId].checked === true) {
+                state[cartItemId].checked = false;
+            } else {
+                state[cartItemId].checked = true;
+            }
+        },
+        checkAllItem(state, action) {
+            let checkLength = action.payload;
+            let cartLength = state.length;
+            if (checkLength > 0) {
+                for (let i = 0; i < checkLength; i++) {
+                    state[i].checked = false;
+                }
+            } else {
+                for (let i = 0; i < cartLength; i++) {
+                    state[i].checked = true;
+                }
+            }
+        },
     },
-    dropItem(state, action){
-      state.pop(action.payload)
-    },
-    increase(state, action){
-      let cartItemId = state.findIndex((data) => { return data.id === action.payload})
-      state[cartItemId].count++
-    },
-    decrease(state, action){
-      let cartItemId = state.findIndex((data) => { return data.id === action.payload})
-      if (state[cartItemId].count > 0) {
-        state[cartItemId].count--
-      }
-    },
-    checkItem(state, action){
-      let cartItemId = state.findIndex((data) => { return data.id === action.payload})
-      if (state[cartItemId].checked === true) {
-        state[cartItemId].checked = false
-      } else {
-        state[cartItemId].checked = true
-      }
-    },
-    checkAllItem(state, action){
-      let checkLength = action.payload
-      let cartLength = state.length
-      if (checkLength > 0) {
-        for (let i = 0; i < checkLength; i++) {
-          state[i].checked = false
-        }
-      } else {
-          for (let i = 0; i < cartLength; i++) {
-            state[i].checked = true
-          }
-      }
-    }
-  }
-})
+});
 
-export const { addItem, dropItem, increase, decrease, checkItem, checkAllItem } = cartSlice.actions
+export const { addItem, dropItem, increase, decrease, checkItem, checkAllItem } = cartSlice.actions;
 
 export default cartSlice;
